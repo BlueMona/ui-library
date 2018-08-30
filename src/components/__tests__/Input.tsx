c4bf9960f33f296c4cf4b6cf32ed19f635c1311a
six <lostfictions@users.noreply.github.com>
2018-08-30 15:24:13 -0400

    make tests compile properly, add snapshot testing
    unfortunately they get included in dist/ now, but that doesn't hurt anything since we use the whole repo including all of src/ as a dep anyway...

--- src/components/__tests__/Input.tsx
+++ src/components/__tests__/Input.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 
-import { Input } from './Input';
+import { Input } from '../Input';
 
 describe('<Input>', () => {
-  it('should default to <input> if multiline is not specified', () => {
+  it('should match the snapshot', () => {
+    expect(shallow(<Input value="Hello input!" />)).toMatchSnapshot();
+  })
+
+  it('should contain a dom <input> if multiline is not specified', () => {
     expect(shallow(<Input />).find('input')).toExist();
   })
 
-  it('should be a <textarea> if multiline is specified', () => {
+  it('should contain a dom <textarea> if multiline is specified', () => {
     expect(shallow(<Input multiline />).find('textarea')).toExist();
   })
 
