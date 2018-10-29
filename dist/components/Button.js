e665047371d997d73ff6cc0677121bb8f1a4c372
Alexandra Marin <alexandra.marin01@gmail.com>
2018-10-29 11:44:09 +0100

    fix: dist

--- dist/components/Button.js
+++ dist/components/Button.js
@@ -26,13 +26,13 @@ class Button extends react_1.Component {
         const buttonContent = [
             this.props.icon && (react_1.default.createElement(MaterialIcon_1.MaterialIcon, { key: `button-material-icon-${this.props.icon}`, icon: this.props.icon })),
             this.props.customIcon && (react_1.default.createElement(CustomIcon_1.CustomIcon, { key: `button-custom-icon-${this.props.customIcon}`, icon: this.props.customIcon })),
-            this.props.label || this.props.children ? (react_1.default.createElement("span", { key: `button-label-${this.props.label || this.props.children}`, "data-test-id": this.props.testId, className: "label" }, this.props.label || this.props.children)) : null,
+            this.props.label || this.props.children ? (react_1.default.createElement("span", { key: `button-label-${this.props.label || this.props.children}`, className: "label" }, this.props.label || this.props.children)) : null,
             this.props.tooltip ? (react_1.default.createElement(Tooltip_1.Tooltip, { key: `button-tooltip-${this.props.tooltip}`, text: this.props.tooltip, position: this.props.tooltipPosition || 'top', size: this.props.tooltipSize })) : null
         ];
         if (this.props.href) {
             return (react_1.default.createElement("a", { tabIndex: this.props.tabIndex, href: this.props.href, className: classNames, onMouseEnter: this.props.onMouseEnter, onMouseLeave: this.props.onMouseLeave, style: this.props.style }, buttonContent));
         }
-        return (react_1.default.createElement("button", { tabIndex: this.props.tabIndex, className: classNames, onClick: this.props.onClick, onMouseEnter: this.props.onMouseEnter, onMouseLeave: this.props.onMouseLeave, disabled: this.props.disabled, style: this.props.style }, buttonContent));
+        return (react_1.default.createElement("button", { "data-test-id": this.props.testId, tabIndex: this.props.tabIndex, className: classNames, onClick: this.props.onClick, onMouseEnter: this.props.onMouseEnter, onMouseLeave: this.props.onMouseLeave, disabled: this.props.disabled, style: this.props.style }, buttonContent));
     }
 }
 exports.Button = Button;
