169649cc1c4eb06c2085cff23e58857c56ecb1bc
suprko <lucas@peerio.com>
2018-11-12 17:15:04 -0500

    update peer-ui.ts

--- src/peer-ui.ts
+++ src/peer-ui.ts
@@ -1,28 +1,29 @@
-// // Inputs & other interactables
+// Inputs & other interactables
 export * from './components/Button';
 export * from './components/Checkbox';
 export * from './components/Dropdown';
 export * from './components/Input';
 export * from './components/RadioButtons';
+export * from './components/SearchInput';
 export * from './components/Switch';
 
-// // Icons
+// Icons
 export * from './components/Avatar';
 export * from './components/CustomIcon';
 export * from './components/MaterialIcon';
 
-// // Informational
+// Informational
 export * from './components/Chip';
 export * from './components/Dialog';
 export * from './components/Tooltip';
 export * from './components/ProgressBar';
 
-// // Navigation
+// Navigation
 export * from './components/Menu';
 export * from './components/MenuHeader';
 export * from './components/MenuItem';
 
-// // Page structure & layout
+// Page structure & layout
 export * from './components/Divider';
 export * from './components/List';
 export * from './components/ListHeading';
