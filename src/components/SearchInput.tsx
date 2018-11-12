b552ca1422f6db3603572f8c4d987b7cd3a83e53
suprko <lucas@peerio.com>
2018-11-12 17:16:14 -0500

    update SearchInput types

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Input } from './Input';
 
-export default class SearchInput extends React.Component {
+export default class SearchInput extends React.Component<Input> {
   render() {
     return <Input {...this.props} isSearch={true} />;
   }
