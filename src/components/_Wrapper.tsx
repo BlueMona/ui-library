9fb3d8691b73da6e01b33355977957f20926473b
suprko <lucas@peerio.com>
2018-11-16 12:37:34 -0500

    explanatory comment on wrapper

--- src/components/_Wrapper.tsx
+++ src/components/_Wrapper.tsx
@@ -1,3 +1,18 @@
+/**
+ * This class is intended to wrap the entire app (or at least the portion
+ * of it that contains the UI) to provide some context or functionality
+ * for PeerUI. Wrapping like this allows us to define certain things that
+ * need to be global-ish on the client side, but affect PeerUI.
+ *
+ * Currently all it contains is a listener that adds or removes a
+ * `.keyboard-nav` className on the wrapper div, based on user keyboard/mouse
+ * input. This in turn applies outlines and other styles to active/focused
+ * elements.
+ *
+ * Eventually there may be things like theming, or portal management, so
+ * we've given it a generic name.
+ */
+
 import React from 'react';
 import { action, computed, observable } from 'mobx';
 import _ from 'lodash';
