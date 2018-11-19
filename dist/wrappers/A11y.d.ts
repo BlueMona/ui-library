f79e548506a4a78b945d20cdb0145355b859e7f1
suprko <lucas@peerio.com>
2018-11-19 12:34:05 -0500

    updated comments

--- dist/wrappers/A11y.d.ts
+++ dist/wrappers/A11y.d.ts
@@ -1,16 +1,12 @@
 /**
  * This class is intended to wrap the entire app (or at least the portion
- * of it that contains the UI) to provide some context or functionality
- * for PeerUI. Wrapping like this allows us to define certain things that
- * need to be global-ish on the client side, but affect PeerUI.
+ * of it that contains the UI) to provide some control for global-ish state
+ * that affects accessibility of PeerUI components.
  *
- * Currently all it contains is a listener that adds or removes a
+ * Currently all it contains is listeners that add or remove a
  * `.keyboard-nav` className on the wrapper div, based on user keyboard/mouse
  * input. This in turn applies outlines and other styles to active/focused
  * elements.
- *
- * Eventually there may be things like theming, or portal management, so
- * we've given it a generic name.
  */
 import React from 'react';
 export declare class A11yWrapper extends React.Component {
--- dist/wrappers/A11y.js
+++ dist/wrappers/A11y.js
@@ -1,17 +1,13 @@
 "use strict";
 /**
  * This class is intended to wrap the entire app (or at least the portion
- * of it that contains the UI) to provide some context or functionality
- * for PeerUI. Wrapping like this allows us to define certain things that
- * need to be global-ish on the client side, but affect PeerUI.
+ * of it that contains the UI) to provide some control for global-ish state
+ * that affects accessibility of PeerUI components.
  *
- * Currently all it contains is a listener that adds or removes a
+ * Currently all it contains is listeners that add or remove a
  * `.keyboard-nav` className on the wrapper div, based on user keyboard/mouse
  * input. This in turn applies outlines and other styles to active/focused
  * elements.
- *
- * Eventually there may be things like theming, or portal management, so
- * we've given it a generic name.
  */
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
